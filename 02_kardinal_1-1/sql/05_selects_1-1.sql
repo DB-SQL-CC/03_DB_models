d038e932d1ed29a45398268c11dda949337172f5
Peter <schmidt.multimedia@gmail.com>
2023-09-06 13:02:25 +0200

    02 / 1-1 / INNER JOIN

--- 02_kardinal_1-1/sql/05_selects_1-1.sql
+++ 02_kardinal_1-1/sql/05_selects_1-1.sql
@@ -1,1 +1,25 @@
--- to do!
+-- SELECTS 1:1
+
+-- Einzeltabellen
+SELECT * FROM mydb.cats;
+SELECT * FROM mydb.servants;
+
+-- Kreuzprodukt (Kartesisches Produkt)
+SELECT * FROM mydb.cats JOIN mydb.servants;
+
+-- Kreuzprodukt (gefiltert)
+SELECT * FROM mydb.cats JOIN mydb.servants
+#WHERE cat_name = "Grizabella"  -- scharf
+#WHERE cat_name LIKE "Gri%"  -- unscharf
+WHERE cat_name LIKE "_ri%"
+;
+
+-- Inner Join 1 / Gesamte Tabelle
+SELECT
+	*
+FROM mydb.cats INNER JOIN mydb.servants
+-- Werte Primärschlüssel/MT = Werte Fremdschlüssel/DT
+ON mydb.cats.id = mydb.servants.cats_id
+;
+
+
--- 04_kardinal_n-m/sql/02_tabs_w_inserts.sql
+++ 04_kardinal_n-m/sql/02_tabs_w_inserts.sql
@@ -46,3 +46,38 @@ INSERT INTO `mydb`.`products` (`id`, `product_name`, `product_price`) VALUES (DE
 -- Products: Inhalte 
 SELECT * FROM mydb.products;
 
+/*  PURCHASES (Kaufprozesse)*/
+
+-- ServantsProducts (purchases)
+CREATE TABLE IF NOT EXISTS `mydb`.`purchases` (
+  `servants_id` INT NOT NULL,
+  `products_id` INT NOT NULL,
+  PRIMARY KEY (`servants_id`, `products_id`),
+  INDEX `fk_servants_has_products_products1_idx` (`products_id` ASC),
+  INDEX `fk_servants_has_products_servants_idx` (`servants_id` ASC),
+  CONSTRAINT `fk_servants_has_products_servants`
+    FOREIGN KEY (`servants_id`)
+    REFERENCES `mydb`.`servants` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_servants_has_products_products1`
+    FOREIGN KEY (`products_id`)
+    REFERENCES `mydb`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- Purchases: Struktur
+DESCRIBE mydb.purchases;
+
+-- Purchases: Inserts (Kaufprozesse : Käufer - Produkt)
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (1, 2);
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (1, 3);
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (2, 1);
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (2, 2);
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (2, 3);
+INSERT INTO `mydb`.`purchases` (`servants_id`, `products_id`) VALUES (2, 4);
+
+-- Purchases: Inhalte 
+SELECT * FROM mydb.purchases;
+
